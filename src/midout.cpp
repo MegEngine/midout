37c924043914f7a568cfcd2d89223e5cd790f71c
王博文 <wangbowen02@megvii.com>
2020-09-15 14:45:20 +0800

    add license for open source

--- src/midout.cpp
+++ src/midout.cpp
@@ -1,6 +1,17 @@
+/**
+ * \file src/midout.cpp
+ * MegEngine is Licensed under the Apache License, Version 2.0 (the "License")
+ *
+ * Copyright (c) 2014-2020 Megvii Inc. All rights reserved.
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT ARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ */
+
 #include "midout.h"
 
-// to avoid linking error for empty lib on some stupid platforms
+// to avoid linking error for empty lib on some platforms
 void midout_empty() {
 }
 
